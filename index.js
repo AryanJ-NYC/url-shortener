ef61bfa778c72c9dc6cd2f0530109192491f0217
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-06-01 23:29:01 -0400

    Grab url parameter with forward slashes; fix urlRegex; match url against urlRegex

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const express = require('express'),
       portNumber = process.env.PORT || process.argv[2] || 8080;
 
 app
-.get('/new/:url', function (req, res) {
-
+.get(/new\/(.+)/, function (req, res) {
+  let urlRegex = /^https?:\/\/(\w+\.)+\w+/,
+      url = req.params[0];
+  if (urlRegex.test(url)) {
+    res.send("Matched");
+  } else {
+    res.json({error: "Wrong url format, make sure you have a valid protocol and real site."});
+  }
 })
 .get('/:id', function (req, res) {
 
 });
 
-app.listen(portNumber);
+app.listen(portNumber, function () {
+  console.log("Listening on port " + portNumber);
+});
