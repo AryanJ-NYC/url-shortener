3ca933d4a2e624db8b6c24adcb511d593c2a8472
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-06-02 23:29:19 -0400

    Handle duplicate urls

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ autoIncrement.initialize(db);
 let urlSchema = new Schema({
   url : { type: String, index : { unique : true } }
 });
+urlSchema.index({ url: 1 });
 urlSchema.plugin(autoIncrement.plugin, { model: 'Url', field: 'urlId', startAt: 1 });
 let Url = db.model('Url', urlSchema);
 
@@ -21,18 +22,39 @@ app
 .get(/\/new\/(.+)/, function (req, res) {
   let urlRegex = /^https?:\/\/(\w+\.)+\w+/,
       urlString = req.params[0];
-  if (urlRegex.test(urlString)) {
-    let url = new Url({ url: urlString });
-    url.save(function (err) {
-      if (err) {
-        console.log(err.message);
+  Url.findOne({ url: urlString }, function (err, result) {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    } else {
+      if (result) {
+        // send the result if the url passed in is matched
+        res.send({
+          url: result.url,
+          urlId: result.urlId,
+          _id: result._id
+        });
       } else {
-        res.send(url);
+        // if not found in database, save url
+        if (urlRegex.test(urlString)) {
+          let url = new Url( {url: urlString} );
+          url.save(function (err) {
+            if (err) {
+              console.log(err.message);
+            } else {
+              res.send({
+                url: result.url,
+                urlId: result.urlId,
+                _id: result._id
+              });
+            }
+          });
+        } else {
+          res.json({error: "Wrong url format, make sure you have a valid protocol and real site."});
+        }
       }
-    });
-  } else {
-    res.json({error: "Wrong url format, make sure you have a valid protocol and real site."});
-  }
+    }
+  });
 })
 .get(/\/(\d)/, function (req, res) {
   let id = req.params[0];
@@ -41,7 +63,7 @@ app
       console.log(err.message);
       throw err;
     } else {
-      if (!result) {
+      if (! result) {
         res.send("Null id");
       } else {
         res.redirect(result.url);
